6762daefbb2c40c4ba505e0c33a4ceed6712a597
GZMaster <33158650+GZMaster@users.noreply.github.com>
2022-12-02 22:52:01 +0100

    two strings close attempt not solved yet

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
+        String input1 = "abbzzca";
+        String input2 = "babzzcz";
 
+        boolean findCloseStrings = new TwoStringsClose().closeStrings(input1, input2);
+
+        System.out.println(findCloseStrings);
     }
 }
--- src/TwoStringsClose.java
+++ src/TwoStringsClose.java
@@ -1,8 +1,39 @@
+import java.util.*;
+
 public class TwoStringsClose {
 
     public boolean closeStrings(String word1, String word2) {
+        String[] word1Arr = word1.split("");
+        String[] word2Arr = word2.split("");
 
+        boolean result = false;
 
-        return true;
+        HashMap<String, Integer> setA = new HashMap();
+        HashMap<String, Integer> setB = new HashMap();
+
+        for (int i = 0; i < word1Arr.length; i++) {
+            increamentvalue(setA, word1Arr[i]);
+        }
+
+        for (int i = 0; i < word2Arr.length; i++) {
+            increamentvalue(setB, word2Arr[i]);
+        }
+
+        if (word1Arr.length == word2Arr.length) {
+            result = setB.equals(setA);
+        } else {
+            return false;
+        }
+        return result;
+    }
+
+    public static<K> void increamentvalue(Map<K, Integer> map, K key) {
+        Integer count = map.get(key);
+
+        if (count == null) {
+            map.put(key, 1);
+        } else {
+            map.put(key, count + 1);
+        }
     }
 }
